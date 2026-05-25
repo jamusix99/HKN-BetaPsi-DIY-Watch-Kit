ab90cb1f4239d0811571a6122d50de9addcef77d
jamusix99 <jmentgen.student@gmail.com>
2026-05-25 17:08:07 -0500

    HW Rev2 + SW Update
    Attempt at a rev2 + some starter code for working with the AVR uC's.
    
    Tags:
     * Type: Feature

--- SW/main.c
+++ SW/main.c
@@ -1,1 +1,17 @@
 // TODO: write code for watch
+#define F_CPU 16000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    DD0 |= (1 << PIN2);
+
+    while(1)
+    {
+        PORT0 ^= (1 << PIN2);
+        _delay_ms(500);
+    }
+}
